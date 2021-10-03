029f7d4cb0b0db87dc9f4a1f8937040508ec0da5
nguyenanh1609 <nguyenhuuanh102000@gmail.com>
2021-10-03 10:41:40 +0700

    first commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,12 +10,11 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
-import { GlobalStyle } from '../styles/global-styles';
-
+import { GlobalStyle } from 'styles/global-styles';
 import { HomePage } from './pages/HomePage/Loadable';
-import { NotFoundPage } from './pages/NotFoundPage/Loadable';
+import { NotFoundPage } from './components/NotFoundPage/Loadable';
 import { useTranslation } from 'react-i18next';
-
+import 'bootstrap/dist/css/bootstrap.css';
 export function App() {
   const { i18n } = useTranslation();
   return (
@@ -27,9 +26,8 @@ export function App() {
       >
         <meta name="description" content="A React Boilerplate application" />
       </Helmet>
-
       <Switch>
-        <Route exact path={process.env.PUBLIC_URL + '/'} component={HomePage} />
+        <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
