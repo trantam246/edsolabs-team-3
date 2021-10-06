867dae26a8d03ffe0d1c26f5fdda21db8d8c0915
nguyenanh1609 <nguyenhuuanh102000@gmail.com>
2021-10-06 07:40:17 +0700

    bug splide cần fix

--- src/app/containers/silders/SlidersList.tsx
+++ src/app/containers/silders/SlidersList.tsx
@@ -1,4 +1,4 @@
-import { Splide, SplideSlide } from '@splidejs/react-splide';
+// import { Splide, SplideSlide } from '@splidejs/react-splide';
 import slide_1 from '../../../images/slide_1.png';
 import slide_2 from '../../../images/slide_2.png';
 import slide_3 from '../../../images/slide_3.png';
@@ -47,49 +47,50 @@ const SlidersList: React.FC = () => {
       },
     },
   };
-
-  const Sliders = styled(Splide)`
-    .splide {
-      &__arrow {
-        width: 7.4rem;
-        height: 7.4rem;
-        transform: translateY(-100%);
-        background: #c9cacd;
-        opacity: 0.1;
-        & svg {
-          width: 1.3rem;
-          height: 2.4rem;
-          color: #dba83d;
-        }
-        &--prev {
-          left: -10rem;
-        }
-        &--next {
-          right: -10rem;
-        }
-      }
-      &__list {
-        width: 100%;
-        justify-content: space-between;
-        margin: 0 auto !important;
-      }
-      &__slide {
-        width: calc(100% / 3 - 2rem) !important;
-        @media screen and (max-width: 375px) {
-          width: 28.3rem;
-          height: 17.7rem;
-        }
-      }
-    }
-  `;
+  // const Sliders = styled(Splide)`
+  //   .splide {
+  //     &__arrow {
+  //       width: 7.4rem;
+  //       height: 7.4rem;
+  //       transform: translateY(-100%);
+  //       background: #c9cacd;
+  //       opacity: 0.1;
+  //       & svg {
+  //         width: 1.3rem;
+  //         height: 2.4rem;
+  //         color: #dba83d;
+  //       }
+  //       &--prev {
+  //         left: -10rem;
+  //       }
+  //       &--next {
+  //         right: -10rem;
+  //       }
+  //     }
+  //     &__list {
+  //       width: 100%;
+  //       justify-content: space-between;
+  //       margin: 0 auto !important;
+  //     }
+  //     &__slide {
+  //       width: calc(100% / 3 - 2rem) !important;
+  //       @media screen and (max-width: 375px) {
+  //         width: 28.3rem;
+  //         height: 17.7rem;
+  //       }
+  //     }
+  //   }
+  // `;
   return (
-    <Sliders options={primaryOptions}>
-      {items.map((item, idx) => (
-        <SplideSlide key={idx}>
-          <SliderItem item={item} />
-        </SplideSlide>
-      ))}
-    </Sliders>
+    <>
+      {/* <Sliders options={primaryOptions}>
+        {items.map((item, idx) => (
+          <SplideSlide key={idx}>
+            <SliderItem item={item} />
+          </SplideSlide>
+        ))}
+      </Sliders> */}
+    </>
   );
 };
 export default SlidersList;
